8df35de1dfce2e26d5b19fe5c57c2452e57efc02
Joseph Haaga <haaga.joe@gmail.com>
2016-04-25 09:17:53 -0400

    Update wordpressPosts.js
    revert to haagajoe wordpress

--- js/MyWordpressPosts/wordpressPosts.js
+++ js/MyWordpressPosts/wordpressPosts.js
@@ -7,7 +7,7 @@ MyWordpressPosts
 
 
 var request2 = new XMLHttpRequest();
-    request2.open("GET", "https://public-api.wordpress.com/rest/v1.1/sites/www.devcurio.us/posts", true);
+    request2.open("GET", "https://public-api.wordpress.com/rest/v1.1/sites/haagajoe.wordpress.com/posts", true);
    console.log(request2.responseText);
     request2.onreadystatechange = function() {//Call a function when the state changes.
         if (request2.readyState == 4 && request2.status == 200) {
